a8780b065d36badf64a682f0ef7216c117b89113
Odilf <odysseas.maheras@gmail.com>
2022-12-01 11:24:49 +0100

    updated util

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 using HTTP
 
-const session_cookie = "53616c7465645f5fc14484efb11094ab73ec11289ab7e007de3898cf2b0bbc5adcd60ea1a47da97b4a1bb14f76296bebb0e427c1609827c45fee271bbf187219"
+include("private.jl")
 
 function get_puzzle_input(day::Integer)
 	response = HTTP.get(
