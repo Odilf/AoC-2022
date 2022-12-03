8ada867d928c12df8278032bed1b5bbb3869ac1e
Odilf <odysseas.maheras@gmail.com>
2022-12-03 11:12:09 +0100

    cache puzzle inputs

--- src/utils.jl
+++ src/utils.jl
@@ -2,11 +2,23 @@ using HTTP
 
 include("private.jl")
 
-function get_puzzle_input(day::Integer)
+filename(day::Integer) = "inputs/day$day"
+
+function update_puzzle_input(day::Integer)
+	@info "Fetching input for day $day"
 	response = HTTP.get(
 		"https://adventofcode.com/2022/day/$day/input"; 
 		headers=["Cookie" => "session=$session_cookie"]
 	)
 
-	return response.body |> String
+	mkpath(dirname(filename(day)))
+	write(filename(day), response.body |> String)
+end
+
+function get_puzzle_input(day::Integer)
+	if !isfile(filename(day))
+		update_puzzle_input(day)
+	end
+
+	return read(filename(day))
 end
