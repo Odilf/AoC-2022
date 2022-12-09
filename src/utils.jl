d9a9576cab43cf432232ab6b72e3b0b7c302a79f
Odilf <odysseas.maheras@gmail.com>
2022-12-09 12:08:04 +0100

    added a `force_update` option and a `print_solution` function

--- src/utils.jl
+++ src/utils.jl
@@ -2,7 +2,9 @@ using HTTP
 
 include("private.jl")
 
-filename(day::Integer) = "inputs/day$day"
+filename(day::Integer) = "$(@__DIR__)/../inputs/day$day"
+
+filename(3)
 
 function update_puzzle_input(day::Integer)
 	@info "Fetching input for day $day"
@@ -15,10 +17,32 @@ function update_puzzle_input(day::Integer)
 	write(filename(day), response.body |> String)
 end
 
-function get_puzzle_input(day::Integer)
-	if !isfile(filename(day))
+function get_puzzle_input(day::Integer, force_update=false)
+	if !isfile(filename(day)) || force_update
 		update_puzzle_input(day)
 	end
 
 	return read(filename(day)) |> String
+end	
+
+function print_solution(part1, part2, input::String)
+	sol1 = nothing
+	sol2 = nothing
+	try
+		sol1 = part1(input)
+	catch
+		@warn "Solution 1 doesn't work"
+	end
+
+	try
+		sol2 = part2(input)
+	catch
+		@warn "Solution 2 doesn't work"
+	end
+
+	@info """
+	The solutions are:
+		- part 1: $sol1
+		- part 2: $sol2
+	"""
 end
